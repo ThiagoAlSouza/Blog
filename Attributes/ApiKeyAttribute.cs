3db7a6b5666bbcf9bace6e89084444a539ae0dd7
Thiago Alves <thiagosouza98@outlook.com.br>
2022-07-19 12:55:33 -0300

    Implementação ApiKeyAttribute

---
+++ Attributes/ApiKeyAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Blog.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class ApiKeyAttribute : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.HttpContext.Request.Query.TryGetValue(Configuration.ApiKeyName, out var extractedApiKey))
+        {
+            context.Result = new ContentResult()
+            {
+                StatusCode = 404,
+                Content = "ApiKey not found."
+            };
+        }
+
+        if (!Configuration.ApiKey.Equals(extractedApiKey))
+        {
+            context.Result = new ContentResult()
+            {
+                StatusCode = 403,
+                Content = "Access unauthorized"
+            };
+        }
+        else
+        {
+           await next();
+        }
+    }
+}
--- Configuration.cs
+++ Configuration.cs
@@ -2,5 +2,7 @@
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "JhsG62JHSou3qnahKSLos6FAV";
+    public static string JwtKey { get; set; } = "JhsG62JHSou3qn8hKSLos6FAV";
+    public static string ApiKeyName { get; set; } = "api_key";
+    public static string ApiKey { get; set; } = "SrvG623HS2u3q3ahKSLpl6F1V";
 }
