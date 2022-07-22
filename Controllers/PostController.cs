8393903e17331bb32f2f7caa539e2a7ef1df4fd4
Thiago Alves <thiagosouza98@outlook.com.br>
2022-07-22 00:03:51 -0300

    Implementação ViewModel para retorno dos Posts

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,12 +1,15 @@
 ﻿using Blog.Data;
 using Blog.Models;
 using Blog.ViewModels.Errors;
+using Blog.ViewModels.Posts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("v1")]
 public class PostController : ControllerBase
 {
@@ -15,9 +18,22 @@ public class PostController : ControllerBase
     {
         try
         {
-            var listPosts = await context.Posts.AsNoTracking().ToListAsync();
+            var listPosts = await context.Posts
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Summary = x.Summary,
+                    Slug = x.Slug,
+                    Author = $"{x.Author.Name} - {x.Author.Email}",
+                    Category = x.Category.Name
+                })
+                .ToListAsync();
 
-            return Ok(new ResultViewModel<List<Post>>(listPosts));
+            return Ok(new ResultViewModel<List<ListPostsViewModel>>(listPosts));
         }
         catch (ArgumentNullException)
         {
---
+++ ViewModels/Posts/ListPostsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.ViewModels.Posts;
+
+public class ListPostsViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Summary { get; set; }
+    public string Slug { get; set; }
+    public string Category { get; set; }
+    public string Author { get; set; }
+}
