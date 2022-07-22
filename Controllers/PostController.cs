10123444aca0aa90464c90b31102fc0a9c6b60be
Thiago Alves <thiagosouza98@outlook.com.br>
2022-07-21 23:45:23 -0300

    Implementação PostController com método getAll simples

---
+++ Controllers/PostController.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Data;
+using Blog.Models;
+using Blog.ViewModels.Errors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Controllers;
+
+[ApiController]
+[Route("v1")]
+public class PostController : ControllerBase
+{
+    [HttpGet("posts")]
+    public async Task<IActionResult> GetAllAsync([FromServices] BlogDataContext context)
+    {
+        try
+        {
+            var listPosts = await context.Posts.AsNoTracking().ToListAsync();
+
+            return Ok(new ResultViewModel<List<Post>>(listPosts));
+        }
+        catch (ArgumentNullException)
+        {
+            return StatusCode(500, new ResultViewModel<List<Category>>("Any value null on object."));
+        }
+    }
+}
