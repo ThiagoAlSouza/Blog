1d2d28d082de59afacf1a53064f78a1218ea04c0
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-10 22:18:46 -0300

    Implementação Post roles

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -1,6 +1,8 @@
 ﻿using Blog.Data;
+using Blog.Extensions;
 using Blog.Models;
 using Blog.ViewModels.Errors;
+using Blog.ViewModels.Roles;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +26,35 @@ public class RoleController : ControllerBase
         catch (ArgumentNullException)
         {
             return StatusCode(500, new ResultViewModel<List<Role>>("Any value null on object."));
+        }
+    }
+
+    [HttpPost("roles")]
+    public async Task<IActionResult> Post([FromServices] BlogDataContext context, [FromBody] EditorRoleViewModel body)
+    {
+        try
+        {
+            if (body == null)
+                return BadRequest(new ResultViewModel<Role>(ModelState.GetErrors()));
+
+            var role = new Role
+            {
+                Name = body.Name,
+                Slug = body.Slug
+            };
+
+            await context.Role.AddAsync(role);
+            await context.SaveChangesAsync();
+
+            return Created($"roles/{role.Id}", role);
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Failed to insert register."));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<Category>("Internal error server"));
         }
     }
 }
