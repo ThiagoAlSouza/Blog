86b202d52ab6f0362c2d67e60e0fc743b9490b36
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-08-12 08:50:41 -0300

    Implementação Get TagController

--- Controllers/TagController.cs
+++ Controllers/TagController.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Data;
+using Blog.Models;
+using Blog.ViewModels.Errors;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Controllers;
 
 [ApiController]
+[Authorize]
+[Route("v1")]
 public class TagController : ControllerBase
 {
+    [HttpGet("tags")]
+    public async Task<IActionResult> Get([FromServices] BlogDataContext context)
+    {
+        try
+        {
+            var listTags = await context.Tag.AsNoTracking().ToListAsync();
 
+            return Ok(new ResultViewModel<List<Tag>>(listTags));
+        }
+        catch (ArgumentNullException)
+        {
+            return StatusCode(500, new ResultViewModel<List<Tag>>("Any value null on object."));
+        }
+    }
 }
