89a1091ae993241b8b923f6046b4bdb562a5ceb2
Thiago Alves <thiagoalsouza98@outlook.com.br>
2022-09-01 23:46:08 -0300

    Implementação interface IRepositoryCategory

---
+++ Interfaces/IRepositoryCategory.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models;
+
+namespace Blog.Interfaces;
+
+public interface IRepositoryCategory
+{
+    #region Methods
+
+    Task<IEnumerable<Category>> GetAll();
+    Task<Category> GetById(int id);
+    Task Save(Category category);
+    Task Update(Category category);
+    Task Delete(int id);
+
+    #endregion
+}
